bb4d1bf468561fc278fd75caa233aac9f92133b0
DAT <terlovoy77@gmail.com>
2024-10-07 16:55:46 +0300

    fix stats page

--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { getSummaryStats } from '@/lib/api';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
 import StatCard, { StatCardType } from '@/app/components/stat-card';
-import Link from 'next/link';
 
 export interface PageProps {}
 
@@ -16,20 +15,20 @@ export default async function Page({}: PageProps) {
   const data = await getSummaryStats({
     // встановлення для next часу кешування ресурсу
     next: {
-      revalidate: 5, //revalidate - Значение в сек. определяет, как часто данные извлекаются повторно. Если нужно сделать кеш недействительным вручную, revalidate: 0
+      revalidate: 5, // revalidate - Значение в сек. определяет, как часто данные извлекаются повторно. Если нужно сделать кеш недействительным вручную, revalidate: 0
     },
   });
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {Object.keys(data).map((key) => (
-        <Link href={`/dashboard/${key}`} key={key} className="col-span-3">
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
+        <div key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
             label={labelByStat[key]}
             counter={data[key]}
           />
-        </Link>
+        </div>
       ))}
     </div>
   );
