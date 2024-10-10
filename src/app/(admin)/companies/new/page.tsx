ba4b3f559be3f6dddc4da77dad68ef8ef0cde374
DAT <terlovoy77@gmail.com>
2024-10-10 16:03:17 +0300

    add dynamically importing CompanyForm

--- src/app/(admin)/companies/new/page.tsx
+++ src/app/(admin)/companies/new/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import React from 'react';
-import CompanyForm from '@/app/components/company-form';
+// import CompanyForm from '@/app/components/company-form';
 
-export interface Props {}
+// Dynamically import the CompanyForm, client-side only
+const CompanyForm = dynamic(() => import('@/app/components/company-form'), {
+  ssr: false,
+});
 
-export default function Page({}: Props) {
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
   return (
     <div className="py-6 px-10">
       <CompanyForm onSubmit={console.log} />
