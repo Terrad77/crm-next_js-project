3d3662a8fa105a1dff31ee6d7600caba346d9be4
DAT <terlovoy77@gmail.com>
2024-10-29 14:55:34 +0200

    add button to Home page

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -9,9 +10,16 @@ export interface ButtonProps
 }
 
 export default function Button({ disabled, ...rest }: ButtonProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/dashboard');
+  };
+
   return (
     <button
       {...rest}
+      onClick={handleClick}
       className={clsx(
         'py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded',
         !disabled && 'hover:bg-gray-800 active:bg-gray-950',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Button from '@/app/components/button';
+
 export default function Home() {
   return (
-    <main>
+    <main className="text-center">
       <h1 className="text-xl text-center mb-4">Home page</h1>
+      <Button>Go to Dashboard</Button>
     </main>
   );
 }
